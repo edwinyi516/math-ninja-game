671d0117dcf89d695cb2aa94c74250ad4b0065e7
Edwin <edwin.yi.128@gmail.com>
2022-07-12 18:08:34 -0500

    Basic HTML and JS outline

---
+++ js/app.js
@@ -0,0 +1,36 @@
+//Player 1 Class
+class Player1 {
+    constructor(character, operator, difficulty) {
+        this.character = character
+        this.operator = operator
+        this.difficulty = difficulty
+    }
+    //Character functions
+
+}
+
+//Player 2 Class
+class Player2 {
+    constructor(character, operator, difficulty) {
+        this.character = character
+        this.operator = operator
+        this.difficulty = difficulty
+    }
+    //Character functions
+
+}
+
+//Instantiate Players
+let player1 = new Player1(player1Character, operatorChoice, difficultyChoice)
+let player2 = new Player2(player2Character, operatorChoice, difficultyChoice)
+
+//Game Object
+let game = {
+    //Game Functions
+
+}
+
+//DOM Elements
+
+
+//Event Listeners

e3bbcfeb7cd621e1afe9c617bbc4a0d83138662c
Edwin <edwin.yi.128@gmail.com>
2022-07-13 18:57:33 -0500

    Added character select screen assets

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-//Player 1 Class
-class Player1 {
+//Player Class
+class Player {
     constructor(character, operator, difficulty) {
         this.character = character
         this.operator = operator
@@ -8,21 +8,6 @@ class Player1 {
     //Character functions
 
 }
-
-//Player 2 Class
-class Player2 {
-    constructor(character, operator, difficulty) {
-        this.character = character
-        this.operator = operator
-        this.difficulty = difficulty
-    }
-    //Character functions
-
-}
-
-//Instantiate Players
-let player1 = new Player1(characterChoice, operatorChoice, difficultyChoice)
-let player2 = new Player2(characterChoice, operatorChoice, difficultyChoice)
 
 //Game Object
 let game = {
@@ -30,6 +15,7 @@ let game = {
     startButton () {
         startScreen.style.display = "none"
         characterSelectScreen.style.display = "flex"
+        player1 = new Player()
     }
 }
 
@@ -41,8 +27,8 @@ const startButton = document.querySelector("#start-button")
 
 const characterSelectScreen = document.querySelector(".character-select-screen")
 
-const chooseMegaMan = document.querySelector("#choose-mega-man")
-const chooseZero = document.querySelector("#choose-zero")
+const chooseMale = document.querySelector("#choose-male")
+const chooseFemale = document.querySelector("#choose-female")
 const chooseAddition = document.querySelector("#choose-addition")
 const chooseSubtraction = document.querySelector("#choose-subtraction")
 const chooseMultiplication = document.querySelector("#choose-multiplication")
